b631559b678d822aaa4474f4d8a0a0ceb2703538
Pedro Rafael <pedrorafael1830@gmail.com>
2026-01-17 18:03:33 -0300

    Adicionando classes de dominio mais básicas

---
+++ src/Ats.WebAPI/Ats.Domain/Entities/Cadidate.cs
@@ -0,0 +1,42 @@
+﻿namespace Ats.Domain.Entities;
+
+public class Candidate : Entity
+{
+    public string Name { get; private set; }
+    public string Email { get; private set; }
+    public int Age { get; private set; }
+    public string? Resume { get; private set; }
+
+    public Candidate(string name, string email, int age, string? resume) : base()
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new ArgumentException("O nome não pode estar vazio.", nameof(name));
+
+        if (age < 18 || age > 65)
+            throw new ArgumentException("A idade deve estar entre 18 e 65 anos.", nameof(age));
+
+        if (!email.Contains('@'))
+            throw new ArgumentException("E-mail inválido.", nameof(email));
+        
+        Name = name;
+        Email = email;
+        Age = age;
+        Resume = resume;
+    }
+
+    public void UpdateInfo(string name, int age, string? resume)
+    {
+        Name = name;
+        Age = age;
+        Resume = resume;
+        UpdateTimestamp();
+    }
+
+    public interface ICandidateRepository
+    {
+        Task AddAsync(Candidate candidate, CancellationToken cancellationToken = default);
+        Task UpdateAsync(Candidate candidate);
+        Task<Candidate> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+        Task<IEnumerable<Candidate>> GetAllAsync();
+    }
+}
---
+++ src/Ats.WebAPI/Ats.Domain/Entities/Entity.cs
@@ -0,0 +1,25 @@
+﻿namespace Ats.Domain.Entities;
+
+public abstract class Entity
+{
+    public Guid Id { get; protected set; }
+    public DateTime CreatedAt { get; protected set; }
+    public DateTime? UpdatedAt { get; protected set; }
+    public DateTime? DeletedAt { get; protected set; }
+    public bool IsDeleted { get; protected set; }
+
+    protected Entity()
+    {
+        Id = Guid.NewGuid();
+        CreatedAt = DateTime.UtcNow;
+        IsDeleted = false;
+    }
+
+    public void Delete()
+    {
+        IsDeleted = true;
+        DeletedAt = DateTime.UtcNow;
+    }
+    
+    public void UpdateTimestamp() => UpdatedAt = DateTime.UtcNow;
+}
