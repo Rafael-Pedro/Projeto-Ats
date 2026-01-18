0584b4dcc4ec382173f5159ed295525ee810e1d2
Pedro Rafael <pedrorafael1830@gmail.com>
2026-01-18 02:01:07 -0300

    Adicionando controller e classe de serializacao

---
+++ src/Ats.WebAPI/Ats.WebAPI/Controllers/CandidateController.cs
@@ -0,0 +1,63 @@
+﻿using Ats.Application.UseCases.Candidates;
+using Mediator;
+using Microsoft.AspNetCore.Mvc;
+using Presentation.Serialization;
+
+namespace Ats.WebAPI.Controllers;
+
+[ApiController]
+[Route("api/candidates")]
+public class CandidateController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public CandidateController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] CreateCandidateCommand request)
+    {
+        var result = await _mediator.Send(request);
+
+        return HttpSerialization.Serialize(result);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetById([FromRoute] Guid id)
+    {
+        var query = new GetCandidateByIdQuery(id);
+        var result = await _mediator.Send(query);
+
+        return HttpSerialization.Serialize(result);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll([FromQuery] int page = 1, [FromQuery] int pageSize = 10)
+    {
+        var query = new GetAllCandidatesQuery(page, pageSize);
+        var result = await _mediator.Send(query);
+
+        return HttpSerialization.Serialize(result);
+    }
+
+    [HttpPut("{id:guid}")]
+    public async Task<IActionResult> Update([FromRoute] Guid id, [FromBody] UpdateCandidateCommand request)
+    {
+        if (id != request.Id)
+            return BadRequest("O ID da URL não coincide com o ID do corpo da requisição.");
+
+        var result = await _mediator.Send(request);
+
+        return HttpSerialization.Serialize(result);
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> Delete([FromRoute] Guid id)
+    {
+        var result = await _mediator.Send(new DeleteCandidateCommand(id));
+
+        return HttpSerialization.Serialize(result);
+    }
+}
---
+++ src/Ats.WebAPI/Ats.WebAPI/Serialization/HttpSerialization.cs
@@ -0,0 +1,69 @@
+﻿using Ats.Application.FluentResultExtensions;
+using FluentResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Serialization;
+
+public class HttpSerialization
+{
+    public static IActionResult Serialize<TValue>(Result<TValue> result)
+    {
+        if (result is { IsSuccess: true })
+            return new ObjectResult(result.Value);
+
+        return Serialize(result.ToResult());
+    }
+
+    public static IActionResult Serialize(Result result)
+    {
+        if (result is { IsSuccess: true })
+            return new NoContentResult();
+
+        var firstError = result.Errors.FirstOrDefault();
+
+        if (firstError is ApplicationError applicationError)
+        {
+            var validationProblemDetails = new ProblemDetails
+            {
+                Detail = applicationError.Message,
+                Status = StatusCodes.Status403Forbidden,
+                Title = applicationError.Message
+            };
+
+            return new ObjectResult(validationProblemDetails) { StatusCode = StatusCodes.Status403Forbidden };
+        }
+
+        if (firstError is RequestValidationError validationError)
+        {
+            var validationProblemDetails = new ValidationProblemDetails(validationError.FieldReasonDictionary)
+            {
+                Detail = firstError.Message,
+                Status = StatusCodes.Status400BadRequest,
+                Title = "Validation error"
+            };
+
+            return new ObjectResult(validationProblemDetails) { StatusCode = StatusCodes.Status400BadRequest };
+        }
+
+        if (firstError is NotFoundError notFoundError)
+        {
+            var validationProblemDetails = new ProblemDetails
+            {
+                Detail = notFoundError.Message,
+                Status = StatusCodes.Status404NotFound,
+                Title = notFoundError.Message
+            };
+
+            return new ObjectResult(validationProblemDetails) { StatusCode = StatusCodes.Status404NotFound };
+        }
+
+        var problemDetails = new ProblemDetails
+        {
+            Detail = firstError?.Message,
+            Status = StatusCodes.Status500InternalServerError,
+            Title = "Unhandled error",
+        };
+
+        return new ObjectResult(problemDetails) { StatusCode = StatusCodes.Status500InternalServerError };
+    }
+}
