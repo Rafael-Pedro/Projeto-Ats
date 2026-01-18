653c8635fa0be339ac8e31375c5e193af174c246
Pedro Rafael <pedrorafael1830@gmail.com>
2026-01-18 17:23:04 -0300

    Adicionando configuração básica de cors

--- src/Ats.WebAPI/Ats.WebAPI/Program.cs
+++ src/Ats.WebAPI/Ats.WebAPI/Program.cs
@@ -18,6 +18,16 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(p =>
+        p.AllowAnyOrigin()
+         .AllowAnyHeader()
+         .AllowAnyMethod());
+});
+
+
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -25,6 +35,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors();
 
 app.UseHttpsRedirection();
 
