ec3cc1c372f74bdb7f850a2d130a6302e9d85155
Pedro Rafael <pedrorafael1830@gmail.com>
2026-01-19 11:28:52 -0300

    criando execption e validação customizados

--- src/Ats.WebAPI/Ats.Application/Behaviours/ExceptionHandlingBehaviour.cs
+++ src/Ats.WebAPI/Ats.Application/Behaviours/ExceptionHandlingBehaviour.cs
@@ -1,11 +1,14 @@
-﻿using FluentResults;
+﻿using Ats.Application.FluentResultExtensions;
+using Ats.Domain.Exceptions;
+using FluentResults;
 using Mediator;
 using Microsoft.Extensions.Logging;
 
 namespace Ats.Application.Behaviours;
+
 public class ExceptionHandlingBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
     where TRequest : IRequest<TResponse>
-    where TResponse : ResultBase<TResponse>, new()
+    where TResponse : ResultBase, new()
 {
     private readonly ILogger<ExceptionHandlingBehaviour<TRequest, TResponse>> _logger;
 
@@ -14,7 +17,7 @@ public class ExceptionHandlingBehaviour<TRequest, TResponse> : IPipelineBehavior
         _logger = logger;
     }
 
-    public async ValueTask<TResponse> Handle(TRequest message, MessageHandlerDelegate<TRequest, TResponse> next, CancellationToken cancellationToken)
+    public async ValueTask<TResponse> Handle(TRequest message,  MessageHandlerDelegate<TRequest, TResponse> next, CancellationToken cancellationToken)
     {
         try
         {
@@ -22,14 +25,22 @@ public class ExceptionHandlingBehaviour<TRequest, TResponse> : IPipelineBehavior
         }
         catch (Exception ex)
         {
-            const string errorMessage = "An unhandled exception has occurred";
+            var response = new TResponse();
 
+            if (ex is DomainException)
+            {
+                _logger.LogWarning("Violação de regra de negócio: {Message}", ex.Message);
+
+                response.Reasons.Add(new ValidationError(ex.Message));
+
+                return response;
+            }
+
+            const string errorMessage = "An unhandled exception has occurred";
             _logger.LogError(ex, "{ErrorMessage}: {Message}", errorMessage, ex.Message);
 
-            var result = new TResponse();
-            result.Reasons.Add(new Error(errorMessage).CausedBy(ex));
-
-            return result;
+            response.Reasons.Add(new Error(errorMessage).CausedBy(ex));
+            return response;
         }
     }
 }
---
+++ src/Ats.WebAPI/Ats.Application/FluentResultExtensions/ValidationError.cs
@@ -0,0 +1,11 @@
+﻿using FluentResults;
+
+namespace Ats.Application.FluentResultExtensions;
+
+public class ValidationError : Error
+{
+    public ValidationError(string message) : base(message)
+    {
+        Metadata.Add("ErrorCode", "VALIDATION_ERROR");
+    }
+}
