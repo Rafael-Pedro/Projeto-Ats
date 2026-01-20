129d7ba7a6ac62e873608ea5ba919e3e11dfbd76
Pedro Rafael <pedrorafael1830@gmail.com>
2026-01-19 21:12:48 -0300

    Adicionando produces para documentação no swagger

--- src/Ats.WebAPI/Ats.WebAPI/Controllers/JobController.cs
+++ src/Ats.WebAPI/Ats.WebAPI/Controllers/JobController.cs
@@ -1,4 +1,5 @@
 ﻿using Ats.Application.UseCases.Jobs;
+using Ats.Domain.Common;
 using Ats.WebAPI.Serialization;
 using Mediator;
 using Microsoft.AspNetCore.Mvc;
@@ -7,6 +8,7 @@ namespace Ats.WebAPI.Controllers;
 
 [ApiController]
 [Route("api/Jobs")]
+[Produces("application/json")]
 public class JobsController : ControllerBase
 {
     private readonly ISender _sender;
@@ -17,6 +19,8 @@ public class JobsController : ControllerBase
     }
 
     [HttpPost]
+    [ProducesResponseType(typeof(CreateJobCommandResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> Create([FromBody] CreateJobCommand request)
     {
         var result = await _sender.Send(request);
@@ -25,6 +29,7 @@ public class JobsController : ControllerBase
     }
 
     [HttpGet]
+    [ProducesResponseType(typeof(PagedResult<GetAllJobsQueryResponse>), StatusCodes.Status200OK)]
     public async Task<IActionResult> GetAll(
         [FromQuery] int page = 1,
         [FromQuery] int pageSize = 10,
@@ -39,6 +44,8 @@ public class JobsController : ControllerBase
     }
 
     [HttpGet("{id:guid}")]
+    [ProducesResponseType(typeof(GetJobByIdResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> GetById([FromRoute] Guid id)
     {
         var query = new GetJobByIdQuery(id);
@@ -47,6 +54,9 @@ public class JobsController : ControllerBase
     }
 
     [HttpPut("{id:guid}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> Update([FromRoute] Guid id, [FromBody] UpdateJobRequest request)
     {
         var command = new UpdateJobCommand(id, request.Title, request.Description, request.Salary);
@@ -55,6 +65,8 @@ public class JobsController : ControllerBase
     }
 
     [HttpDelete("{id:guid}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> Delete([FromRoute] Guid id)
     {
         var command = new DeleteJobCommand(id);
